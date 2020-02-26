a91d792b861e575dab363510c371704060453d99
Iggy Jackson <iggy.jackson@pluto.tv>
2020-02-26 14:31:51 -0800

    Add ability to set env variables in new workspace
    Add code to handle setting multiple env vars via cmd line args and then
    setting or updating those via the terraform cloud API
    
    Signed-off-by: Iggy Jackson <iggy.jackson@pluto.tv>

--- main.go
+++ main.go
@@ -25,12 +25,24 @@ package main
 
 import (
 	"context"
-  "flag"
+	"flag"
 	"log"
-  "os"
+	"os"
+	"strings"
 
 	tfe "github.com/hashicorp/go-tfe"
 )
+
+type sliceFlags []string
+
+func (i *sliceFlags) String() string {
+	return "foo"
+}
+
+func (i *sliceFlags) Set(value string) error {
+	*i = append(*i, strings.TrimSpace(value))
+	return nil
+}
 
 // for future use
 // func workspaceCreate() {
@@ -42,7 +54,9 @@ var organization = flag.String("org", "", "The terraform cloud organization to u
 var workspace = flag.String("workspace", "", "The workspace name.")
 
 func main() {
-  flag.Parse()
+	var envFlags sliceFlags
+	flag.Var(&envFlags, "env", "Environment variables to set on the workspace (-env KEY=Value)")
+	flag.Parse()
 
 	config := &tfe.Config{
 		Token: os.Getenv("TERRAFORM_CLOUD_TOKEN"),
@@ -64,14 +78,27 @@ func main() {
 
 		// Create a new workspace
 		w, err = client.Workspaces.Create(ctx, *organization, tfe.WorkspaceCreateOptions{
-			Name: tfe.String(*workspace),
+			Name:             tfe.String(*workspace),
 			AutoApply:        tfe.Bool(false),
 			TerraformVersion: tfe.String("latest"),
 		})
 		if err != nil {
-			log.Fatal("failed creating workspace: ", err)
+			log.Fatal("failed creating workspace: ", err, w)
 		}
-		log.Print(w)
+
+		// Since the workspace didn't exist, we can assume the ENV vars didn't either
+		for ev := range envFlags {
+			envkv := strings.Split(envFlags[ev], "=")
+			log.Printf("%q\n", envkv)
+			v, err := client.Variables.Create(ctx, w.ID, tfe.VariableCreateOptions{
+				Key:   &envkv[0],
+				Value: &envkv[1],
+			})
+			if err != nil {
+				log.Fatal("failed setting ")
+			}
+			log.Print("v = ", v)
+		}
 
 	} else {
 		log.Print("workspace already exists. Updating attrs if necessary")
@@ -85,6 +112,50 @@ func main() {
 		if err != nil {
 			log.Fatal("failed updating workspace attrs (AutoApply|TerraformVersion): ", err)
 		}
-		log.Print(w)
+
+		// check if the vars exist and create/update appropriately
+		for ev := range envFlags {
+			envkv := strings.Split(envFlags[ev], "=")
+			key := envkv[0]
+			value := envkv[1]
+			foundEnvInVarList := false
+
+			log.Printf("%v:%v\n", key, value)
+			varList, err := client.Variables.List(ctx, w.ID, tfe.VariableListOptions{})
+			if err != nil {
+				log.Fatal("Failed getting list of variables", err)
+			}
+			for li := range varList.Items {
+				log.Printf("li: %v | %v\n", li, varList.Items[li])
+				if varList.Items[li].Key == key {
+					foundEnvInVarList = true
+					if varList.Items[li].Value != value || varList.Items[li].Sensitive != true {
+						// key exists, but value/Sensitive doesn't match, update it
+						vu, err := client.Variables.Update(ctx, w.ID, varList.Items[li].ID, tfe.VariableUpdateOptions{
+							Key:         &key,
+							Value:       &value,
+							Description: tfe.String("Set by terrarific"),
+							Sensitive:   tfe.Bool(true),
+						})
+						if err != nil {
+							log.Fatal("Failed updating existing env var ", err, vu)
+						}
+					}
+				}
+			}
+			if foundEnvInVarList == false {
+				// we never found the env var in the list from tf cloud, create it
+				v, err := client.Variables.Create(ctx, w.ID, tfe.VariableCreateOptions{
+					Key:         &key,
+					Value:       &value,
+					Description: tfe.String("Set by terrarific"),
+					Category:    tfe.Category(tfe.CategoryEnv),
+					Sensitive:   tfe.Bool(true),
+				})
+				if err != nil {
+					log.Fatal("failed setting ", err, v)
+				}
+			}
+		}
 	}
 }
